9dcdf2e94a9f0d702e4307e0197c4a6548ed78c0
Alber Erre <AlberErre@MacBook-Pro-de-Alber.local>
2017-03-14 23:12:00 +0000

    march update

--- linear_nn.py
+++ linear_nn.py
@@ -51,3 +51,44 @@ for step in range(116):
 
 
 
+################################################
+## USING NOISE:
+
+noise = np.random.normal(0, 0.06, x_data.shape)
+y_data_noisy = _realWeights*(x_data + noise) + _realBiases # Y values following a linear structure
+
+plt.scatter(x_data,y_data_noisy)
+plt.show()
+
+#####
+HIGH_noise = np.random.rand(100).astype(np.float32)
+y_data_HIGH_noise = _realWeights*(x_data + HIGH_noise) + _realBiases # Y values following a linear structure
+
+plt.scatter(x_data,y_data_HIGH_noise)
+plt.show()
+
+####
+Loss_error_noisy_HIGH = tf.reduce_mean(tf.square(Predicted_y - y_data_noisy)) 
+    # We can use "y_data_HIGH_noise" instead of "y_data_noisy"
+    # Depends of how much noise you want :D
+optimizer = tf.train.GradientDescentOptimizer(0.5)
+			## We also can use "AdamOptimizer"
+
+trainModel_noisy = optimizer.minimize(Loss_error_noisy_HIGH)
+
+## Run the model and init (HIGH noise)
+init = tf.initialize_all_variables()#, no longer availability after 2017-03-02. 
+#init = tf.global_variables_initializer()
+sess = tf.Session()
+sess.run(init)
+
+# Increasing Range will increase the accuracy of the model
+for step in range(116):
+    sess.run(trainModel_noisy)
+    if step % 15 == 0: # Batch: 15, plot results every 15 observations
+        print("Observation:", step, 
+              "Model Weights", sess.run(_modelWeights),
+              "Model Biases:", sess.run(_modelBiases),
+              "Real Weights:", _realWeights, 
+              "Real Biases:", _realBiases)
+
